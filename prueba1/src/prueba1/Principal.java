3a01ebe08d4a2af13f7824a1ccd571cb494f2cf0
alozanop02 <alozanop02@iesalbarregas.es>
2021-03-01 21:25:21 +0100

    Segundo cambio en principal 3º commit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo java-git");
 		System.out.println("2 Hola mundo java-git");
+		System.out.println("3 Hola mundo java-git");
 	}
 
 }
